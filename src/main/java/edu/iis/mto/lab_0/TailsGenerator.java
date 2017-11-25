e78d634e76cdb731a7aa52a334d64a71edc28e49
Piotr Chojnacki <198144@edu.p.lodz.pl>
2017-11-25 16:35:03 +0100

    lab 0

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -5,9 +5,14 @@ import java.util.List;
 
 public class TailsGenerator {
 
-	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
-		return tails;
-	}
+    public List<String> tails(String value) {
+        List<String> tails = new ArrayList<>(value.length());
+
+        int i;
+        for (i = 0; i < value.length() + 1; i++) {
+            tails.add(value.substring(i, value.length()));
+        }
+        return tails;
+    }
 
 }
